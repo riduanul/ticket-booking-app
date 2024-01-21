b1d1b06fea86c30c7b586daf81c1b4350510b9f3
riduanul <riduanul.haque1@gmail.com>
2024-01-21 21:54:33 +0600

    ui updated

--- train_ticket_booking/settings.py
+++ train_ticket_booking/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ['https://ticket-booking-app-nl0a.onrender.com/', 'https://*.127.0.0.1', 'https://ticket-booking-app-nl0a.onrender.com', 'https://ticket-booking-app-nl0a.onrender']
