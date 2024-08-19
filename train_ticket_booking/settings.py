4b282b6652371c0e9cc4544ac06027a6cb05b7be
riduanul <riduanul.haque1@gmail.com>
2024-08-19 22:54:43 +0600

    email verification removed

--- train_ticket_booking/settings.py
+++ train_ticket_booking/settings.py
@@ -124,9 +124,9 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT =587
-EMAIL_HOST_USER = env('EMAIL')
-EMAIL_HOST_PASSWORD = env('PASSWORD')
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_USE_TLS = True
+# EMAIL_PORT =587
+# EMAIL_HOST_USER = env('EMAIL')
+# EMAIL_HOST_PASSWORD = env('PASSWORD')
